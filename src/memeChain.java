61984755c12575fa3aa0b6de3ecfa4598e77d553
meezumi <aaryank3000@gmail.com>
2024-06-16 22:46:03 +0530

    added google json for json strings of block formatting, using arrayList to store the blocks and isChainValid() method.

--- src/memeChain.java
+++ src/memeChain.java
@@ -3,21 +3,47 @@ import com.google.gson.GsonBuilder;
 
 
 public class memeChain {
-    // here we will create blocks and print hashes to check.
+
+    // will be using arrays for the blocks:
+    public static ArrayList<Block> blockchain = new ArrayList<>();
 
     public static void main(String[] args) {
 
+//        adding the blocks to the arraylist
+        blockchain.add(new Block("this is the first block", "0"));
+        blockchain.add(new Block("its me hi im the second block its me", blockchain.get(blockchain.size()-1).hash));
+        blockchain.add(new Block("ayo its the third block here", blockchain.get(blockchain.size()-1).hash));
 
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
 
-//        first block has previousHash 0 since it's the first block
-        Block genesisBlock = new Block("this is the first block meow", "0");
-        System.out.println("hash for block 1: " + genesisBlock.hash);
+        System.out.println(blockchainJson);
 
-        Block secondBlock = new Block("its me, hi im the second block its me", genesisBlock.hash);
-        System.out.println("hash for block 2: " + secondBlock.hash);
-
-        Block thirdBlock = new Block("ayo im the third block fam", secondBlock.hash);
-        System.out.println("hash for block 3: " + thirdBlock.hash);
+//        the output will now look a lot like a blockChain
+//        now to check the integrity of the blockchain, we create a boolean method isChainValid()
 
     }
+
+    private static Boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+
+//        looping the entire blockchain, i.e. arraylist
+        for(int i=1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i-1);
+
+            if(!currentBlock.hash.equals(currentBlock.calculateHash()) ){
+                System.out.println("current hashes are not equal");
+                return false;
+            }
+
+//            now the same for previous hash and registered previous hash
+            if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
+                System.out.println("previous hashes are not equal");
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
